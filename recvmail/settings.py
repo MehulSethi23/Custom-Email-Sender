125b2575aa09753d3451e3b522c13f3d59c39799
mehul <swagsy2000@gmail.com>
2021-07-16 23:16:42 +0530

    First Commit

--- recvmail/settings.py
+++ recvmail/settings.py
@@ -133,7 +133,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'swagsy2000@gmail.com'
-EMAIL_HOST_PASSWORD = 'manish03'
+EMAIL_HOST_USER = ''  # your email
+EMAIL_HOST_PASSWORD = ''  # your password
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
